49901df452db94a389f0a4f2a2eb7e7523117030
julhoang <64108232+julhoang@users.noreply.github.com>
2022-11-27 14:54:58 -0800

    update surrounding working

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
-GridSize = 16;
+GridSize = 4;
 playerScore = 0;
 computerScore = 0;
-
+gameRunning = 1;
 
 //object for grid of boxes
 const BoxObject = new Object();
@@ -9,72 +9,81 @@ BoxObject.boxNum = 0;
 BoxObject.count = 0;
 BoxObject.sides = new Array(0, 0, 0, 0); //order: up, down, left, right
 
-
 //default to what is being passed in from front end
-const Move = new Object
+const Move = new Object();
 Move.player = "player";
 Move.boxNum1 = -1;
-Move.move1 = -1; //default -1, change to 0,1,2 or 3 
+Move.move1 = -1; //default -1, change to 0,1,2 or 3
 Move.boxNum2 = -1;
-Move.move2 = -1; //default -1, change to 0,1,2 or 3 
-// Move.boxNum3 = -1;
-// Move.move3 = -1; //default -1, change to 0,1,2 or 3
-// Move.boxNum4 = -1;
-// Move.move4 = -1; //default -1, change to 0,1,2 or 3 
-
+Move.move2 = -1; //default -1, change to 0,1,2 or 3
 
 //Convert Move Index To Move Name
 var MoveConversion = new Map([
-	[0, "up"],
-	[1, "down"],
-	[2, "left"],
-	[3, "right"]
+  [0, "up"],
+  [1, "down"],
+  [2, "left"],
+  [3, "right"],
 ]);
 
 //Creating Board
 board = new Array();
 
-function setup(){
-		for(i in range(1,GridSize)){
-			box = BoxObject;
-			board.push(box)
-		}
-	
+function setup() {
+  for (var i = 1; i < GridSize * GridSize; i++) {
+    box = BoxObject;
+    board.push(box);
+  }
+  gameRunning = 1;
+  playerScore = 0;
+  computerScore = 0;
 }
 
+console.log("Hi");
+function test() {
+  setup();
+  console.log("Hi");
+  while (gameRunning) {
+    box = prompt();
+    side = prompt();
+    move = Move;
+    move.sides[side] = 1;
+    move.boxNum1 = box;
+    boardUpdate(move);
+    ComputerMove();
+  }
+}
+test();
 
+function boardUpdate(player_move) {
+  console.log("recieved players move, performing board update");
+  if (player_move.boxNum1 != -1) {
+    board[player_move.boxNum1 - 1].sides[player_move.move1] = 1;
+    board[player_move.boxNum1 - 1].count++;
+    console.log("player's move changes board at box1 and move 1");
+  }
 
-
-
-function boardUpdate(player_move){
-
-  console.log("recieved players move, performing board update")
-	if(player_move.boxNum1 !=-1){
-		board[player_move.boxNum1-1].sides[player_move.move1] = 1;
-		board[player_move.boxNum1-1].count++;
-    console.log("player's move changes board at box1 and move 1")
-	}
-
-  
-  if(player_move.boxNum2 !=-1){
-		board[player_move.boxNum2-1].sides[player_move.move2] = 1;
-		board[player_move.boxNum2-1].count++;    
-    console.log("player's move changes board at box2 and move 2")
+  if (player_move.boxNum2 != -1) {
+    board[player_move.boxNum2 - 1].sides[player_move.move2] = 1;
+    board[player_move.boxNum2 - 1].count++;
+    console.log("player's move changes board at box2 and move 2");
+  }
+  if (player_move.boxNum2 == -1 && player_move.boxNum1 == -1) {
+    console.log("No Move Was Made Due to Impossible Move");
   }
 
   //ALSO ACCOUNT FOR IF PLAYER WINS?????
-full_boxes = 0;
-for(i in board){
-	if(i.count==4){
-	full_boxes += 1;
-	}
-}
-if(full_boxes==GridSize*GridSize){
-	End_Game();
-}
+  full_boxes = 0;
+  for (i in board) {
+    if (i.count == 4) {
+      full_boxes += 1;
+    }
+  }
+  if (full_boxes == GridSize * GridSize) {
+    End_Game();
+  }
 }
 
-//Remove Later - Fixed Move for passed in 
+//Remove Later - Fixed Move for passed in
 passed_in = Move;
 passed_in.boxNum1 = 6;
 passed_in.move1 = 1; //box 1's up
@@ -82,24 +91,21 @@ passed_in.boxNum2 = 2;
 passed_in.move2 = 3; //box 2's down
 
 //pretend passed_in is being passed into boardUpdate
-boardUpdate(passed_in)
-
-
+boardUpdate(passed_in);
 
 //Calculate which sides can be changed
-function possibleMoves(box){
-	moves = new Array();
-	for(let i = 0; i < box.sides.size; i++){
-		if(box.sides[i]==0){
-			moves.push(i);
-		}
-	}
-	return moves;
+//Up Down Left Right
+function possibleMoves(box) {
+  moves = new Array();
+  for (let i = 0; i < box.sides.size; i++) {
+    if (box.sides[i] == 0) {
+      moves.push(i);
+    }
+  }
+  return moves;
 }
 
-function getAttached(curr_box){
-
-  if(curr_box == )
+function getAttached(curr_box) {
   up = curr_box - GridSize;
   down = curr_box + GridSize;
   left = curr_box - 1;
@@ -108,91 +114,72 @@ function getAttached(curr_box){
   return ret_list;
 }
 
-//make computer move after 
-function ComputerMove(){
-  console.log("player's move registered, about to make computer move")
-  newMove = Move
-  newMove.player = "Computer"
-  
+//make computer move after
+function ComputerMove() {
+  console.log("player's move registered, about to make computer move");
+  newMove = Move;
+  newMove.player = "Computer";
+
   for (let i = 0; i < board.size; i++) {
-    if(board[i].count == 3){
-  		console.log("there were 3 lines available, about to make bestMove")
+    if (board[i].count == 3) {
+      console.log("there were 3 lines available, about to make bestMove");
       bestMove(); //called when there is a winning line available
       return; //send back to player
-    }   
+    }
   }
 
-  for (let j = 0; j < board.length; j++) {
-    //j is the curr box
-    if(board[j].count == 1){
-      console.log("count was 1, making random move in this box")
-      //check for attached boxes, if any of them are 2, continue
-      was_two = 000
-      attached = getAttached(j)
-      for(i in attached){
-        if(board[i].count == 2){
-          was_two = 111
-          break
-        }
-      }
-      
-      if(was_two = 111){
-        continue
-      } else{
-        randomMove(j)
-        return;
-      }
-        
-    }   
-  }
-  
+  // for (let j = 0; j < board.length; j++) {
+  // 	//j is the curr box
+  // 	if (board[j].count == 1) {
+  // 		console.log("count was 1, making random move in this box")
+  // 		//check for attached boxes, if any of them are 2, continue
+  // 		was_two = 000
+  // 		attached = getAttached(j)
+  // 		for (i in attached) {
+  // 			if (board[i].count == 2) {
+  // 				was_two = 111
+  // 				break
+  // 			}
+  // 		}
+
+  // 		if (was_two = 111) {
+  // 			continue
+  // 		} else {
+  // 			randomMove(j)
+  // 			return;
+  // 		}
+
+  // 	}
+  // }
 }
 
-
-
-
-function bestMove(){
+function bestMove() {
   //make winning line
-  //recursiveky make lines 
-	for(i in board)
-	for(j in board){
-		if(j.fill == 3){
-			
-		}
-		
-	}
-
+  //recursiveky make lines
+  for (i in board)
+    for (j in board) {
+      if (j.count == 3) {
+        j.count++;
+        computerScore++;
+        move = possibleMoves(j)[0];
+        j.sides[move] = 1;
+        //TODO - Implement touching side increment
+      }
+    }
 }
 
-function randomMove(box_num){
-randMove = Move;
-randMove.boxNum1 = location;
-const randomMove = board[Math.floor(Math.random() * board.length)];
-passed_in.move1 = randomMove;
-passed_in.boxNum2 = neighbour;
-passed_in.move2 = 1;
-	
-	
+// function randomMove(box_num) {
+// 	randMove = Move;
+// 	randMove.boxNum1 = location;
+// 	const randomMove = board[Math.floor(Math.random() * board.length)];
+// 	passed_in.move1 = randomMove;
+// 	passed_in.boxNum2 = neighbour;
+// 	passed_in.move2 = 1;
+
+// }
+
+function reset() {}
+
+function endGame() {
+  gameRunning = 0;
 }
-
-function reset(){
-	
-}
-
-
-
-function endGame(){
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 // FUNCTION CALLING
-const maxCells = 3; // 2 x 2 grid
+const maxCells = 4; // 2 x 2 grid
 var currentPlayer = 1;
 const gameState = [];
 const boxFilled = []; // keep track of which boxes were filled
-const boxNotFilled = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const score1 = 0;
-const score2 = 0;
+const boxNotFilled = Array.from({ length: maxCells * maxCells }, (_, i) => i + 1);
+
+var score1 = 0;
+var score2 = 0;
+
+var move = 1; // odd = human, even = AI
 
 /* 	Sample:
 	gameState = [
-		{box: 1, sides: {left: 1}, filled: true}
+		{box: 1, sides: {left: 1}, filled: 1}
 		{box: 2, sides: {}, filled: false}
 		{box: 3, sides: {}, filled: false}
-		{box: 4, sides: {left: 1}, filled: false}}
+		{box: 4, sides: {left: 1}, filled: 1}}
 	]
 */
 
 initializeGameState();
 createGameBoard();
-
-while (score1 + score2 < 9) {
-  if (currentPlayer == 2) {
-    randomizedMove();
-  }
-}
 
 // FUNCTIONS
 function createGameBoard() {
@@ -51,6 +48,12 @@ function createGameBoard() {
   container.appendChild(table);
 }
 
+function formatTable() {
+  for (var i = 0; i < maxCells * maxCells - maxCells; i++) {
+    document.getElementById("");
+  }
+}
+
 function createCell(cell_counter) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
@@ -58,19 +61,23 @@ function createCell(cell_counter) {
 
   const up = document.createElement("div");
   up.classList.add("up");
-  addOnClick(up, "up", cell_counter);
+  up.classList.add("0");
+  addOnClick(up, 0, cell_counter);
 
   const left = document.createElement("div");
   left.classList.add("left");
-  addOnClick(left, "left", cell_counter);
+  left.classList.add("2");
+  addOnClick(left, 2, cell_counter);
 
   const down = document.createElement("div");
   down.classList.add("down");
-  addOnClick(down, "down", cell_counter);
+  down.classList.add("1");
+  addOnClick(down, 1, cell_counter);
 
   const right = document.createElement("div");
   right.classList.add("right");
-  addOnClick(right, "right", cell_counter);
+  right.classList.add("3");
+  addOnClick(right, 3, cell_counter);
 
   cell.appendChild(up);
   cell.appendChild(left);
@@ -80,48 +87,118 @@ function createCell(cell_counter) {
   return cell;
 }
 
-function addOnClick(element, className, id) {
+function addOnClick(element, side, id) {
   element.addEventListener("click", function (event) {
     // only update gameState if edge is not already clicked
-    if (gameState[id].sides[className] != 1) {
+    if (gameState[id].sides[side] == 0) {
+      console.log("clicked box " + id + "- side " + side);
+      move++;
       currentPlayer = currentPlayer == 1 ? 2 : 1;
-      gameState[id].sides[className] = 1; // add side to gameState
 
       color = currentPlayer == 1 ? "red" : "blue"; // toggle color
       element.style.setProperty("background-color", color);
 
-      if (Object.keys(gameState[id].sides).length == 4) {
-        gameState[id].filled = currentPlayer;
-        boxFilled.push(id);
-        boxNotFilled.pop(id);
+      gameState[id].sides[side] = 1; // update the side that was actually clicked
+      gameState[id].filled++;
 
-        currentPlayer == 1 ? score1++ : score2;
-        document.getElementById(id).style.backgroundColor = color;
-      }
+      checkedFilled(id, color);
+
+      updateSurrounding(id, side, color);
+
+      document.getElementById("currentPlayer").innerHTML = currentPlayer == 1 ? "2" : "1";
     }
   });
 }
 
+function updateSurrounding(id, side, color) {
+  // if we clicked up
+  if (side == 0) {
+    if (id - maxCells > 0) {
+      var newId = id - maxCells;
+      gameState[newId].sides[1] = 1; // force bottom of the the cell above
+      gameState[newId].filled++;
+      document.getElementById(newId).getElementsByClassName("down")[0].style.backgroundColor =
+        color;
+
+      checkedFilled(newId, color);
+    }
+
+    // if we clicked bottom
+  } else if (side == 1) {
+    if (id + maxCells <= maxCells * maxCells) {
+      var newId = id + maxCells;
+      gameState[newId].sides[0] = 1; // force top of the the cell above
+      gameState[newId].filled++;
+      document.getElementById(newId).getElementsByClassName("up")[0].style.backgroundColor = color;
+
+      checkedFilled(newId, color);
+    }
+
+    // if we clicked left
+  } else if (side == 2) {
+    if ((id - 1) % maxCells >= 0) {
+      var newId = id - 1;
+      gameState[newId].sides[3] = 1; // force right of the the cell above
+      gameState[newId].filled++;
+      document.getElementById(newId).getElementsByClassName("right")[0].style.backgroundColor =
+        color;
+
+      checkedFilled(newId, color);
+    }
+
+    // if we clicked right
+  } else {
+    if ((id + 1) % maxCells <= maxCells) {
+      var newId = id + 1;
+      gameState[newId].sides[2] = 1; // force left of the the cell to the right
+      gameState[newId].filled++;
+      document.getElementById(newId).getElementsByClassName("left")[0].style.backgroundColor =
+        color;
+
+      checkedFilled(newId, color);
+    }
+  }
+}
+
 function initializeGameState() {
   for (var i = 1; i <= maxCells * maxCells; i++) {
-    gameState.push({ box: i, sides: {}, filled: 0 });
+    gameState[i] = { box: i, sides: [0, 0, 0, 0], filled: 0 };
+    // gameState.push({ box: i, sides: [0, 0, 0, 0], filled: 0 });
+  }
+
+  console.log(JSON.stringify(gameState, null, 2));
+}
+
+function checkedFilled(id, color) {
+  if (gameState[id].filled == 4) {
+    boxFilled.push(id);
+    boxNotFilled.pop(id);
+    currentPlayer == 1 ? score1++ : score2++;
+    document.getElementById(id).style.backgroundColor = color;
   }
 }
 
 function randomizedMove() {
   const randomIndex = Math.floor(Math.random() * boxNotFilled.length);
-  const moves = possibleMoves(gameState[boxNotFilled[randomIndex]]);
+  const pickedBoxId = boxNotFilled[randomIndex];
+  const moves = possibleMoves(gameState[pickedBoxId], pickedBoxId);
 
-  const pickedMove = Math.floor(Math.random() * moves.length);
-  document.getElementById(boxNotFilled[randomIndex]).click();
+  const pickedSide = Math.floor(Math.random() * moves.length);
+
+  console.log("AI picked box " + boxNotFilled[randomIndex] + " - side: " + moves[pickedSide]);
+  document
+    .getElementById(boxNotFilled[randomIndex])
+    .getElementsByClassName(moves[pickedSide].toString())[0]
+    .click();
 }
 
-function possibleMoves(box) {
+function possibleMoves(box, id) {
   moves = new Array();
-  for (let i = 0; i < Object.keys(gameState[id].sides).length; i++) {
+  for (let i = 0; i < 4; i++) {
+    console.log(JSON.stringify(box));
     if (box.sides[i] == 0) {
       moves.push(i);
     }
   }
-  return moves;
+  return moves; // up, down, left, right
 }
