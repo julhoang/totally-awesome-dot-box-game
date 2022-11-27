ba1c14c109dc23fc924df8de6ffe35ea15a2f514
Khushboo Chugh <92592464+khushbooc8@users.noreply.github.com>
2022-11-27 14:56:48 -0800

    merging with AI

--- backend.js
+++ backend.js
@@ -29,7 +29,7 @@ var MoveConversion = new Map([
 board = new Array();
 
 function setup() {
-  for (var i = 1; i < GridSize * GridSize; i++) {
+  for (var i = 1; i < GridSize*GridSize; i++) {
     box = BoxObject;
     board.push(box);
   }
@@ -38,15 +38,14 @@ function setup() {
   computerScore = 0;
 }
 
-console.log("Hi");
 function test() {
+	console.log("Setting Up");
   setup();
-  console.log("Hi");
   while (gameRunning) {
     box = prompt();
     side = prompt();
     move = Move;
-    move.sides[side] = 1;
+    move.move1 = side;
     move.boxNum1 = box;
     boardUpdate(move);
     ComputerMove();
@@ -67,11 +66,15 @@ function boardUpdate(player_move) {
     board[player_move.boxNum2 - 1].count++;
     console.log("player's move changes board at box2 and move 2");
   }
+  
   if (player_move.boxNum2 == -1 && player_move.boxNum1 == -1) {
     console.log("No Move Was Made Due to Impossible Move");
   }
 
   //ALSO ACCOUNT FOR IF PLAYER WINS?????
+
+
+  //REMOVE?????????? ***********************
   full_boxes = 0;
   for (i in board) {
     if (i.count == 4) {
@@ -106,10 +109,68 @@ function possibleMoves(box) {
 }
 
 function getAttached(curr_box) {
-  up = curr_box - GridSize;
-  down = curr_box + GridSize;
-  left = curr_box - 1;
-  right = curr_box + 1;
+//1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+up = -1;
+down = -1;
+left = -1;
+right = -1;
+  if(curr_box == 6 || curr_box == 7 || curr_box == 10 || curr_box == 11){
+    up = curr_box - GridSize;
+    down = curr_box + GridSize;
+    left = curr_box - 1;
+    right = curr_box + 1;
+   
+  } else if(curr_box == 5 || curr_box == 9){
+    up = curr_box - GridSize;
+    down = curr_box + GridSize;
+    left = -1;
+    right = curr_box + 1;
+    
+  } else if(curr_box == 1){
+    up = -1;
+    down = curr_box + GridSize;
+     left = -1;
+    right = curr_box + 1;
+  }else if(curr_box == 13){
+    up = curr_box - GridSize;
+    down = -1;
+    left = -1;
+    right = curr_box + 1;
+  } else if(curr_box == 4){
+    up = -1;
+    down = curr_box + GridSize;
+    left = curr_box - 1;
+    right = -1;
+  
+  }else if(curr_box == 2 || curr_box == 3){
+    up = -1;
+    down = curr_box + GridSize;
+    left = curr_box - 1;
+    right = curr_box + 1;
+  
+  } else if(curr_box == 3){
+    up = -1;
+    down = curr_box + GridSize;
+    left = curr_box - 1;
+    right = curr_box + 1;
+  
+  } else if(curr_box == 8 || curr_box == 12){
+    up = curr_box - GridSize;
+    down = curr_box + GridSize;
+    left = curr_box - 1;
+    right = -1;
+  } else if(curr_box == 16){
+    up = curr_box - GridSize;
+    down = -1;
+    left = curr_box - 1;
+    right = -1;
+  } else if(curr_box == 14 || curr_box == 15){
+    up = curr_box - GridSize;
+    down = -1;
+    left = curr_box - 1;
+    right = curr_box + 1;
+  }
+  
   ret_list = new Array(up, down, left, right);
   return ret_list;
 }
@@ -117,66 +178,73 @@ function getAttached(curr_box) {
 //make computer move after
 function ComputerMove() {
   console.log("player's move registered, about to make computer move");
-  newMove = Move;
-  newMove.player = "Computer";
 
   for (let i = 0; i < board.size; i++) {
     if (board[i].count == 3) {
       console.log("there were 3 lines available, about to make bestMove");
-      bestMove(); //called when there is a winning line available
-      return; //send back to player
+      bestMove(i); //called when there is a winning line available
     }
   }
 
-  // for (let j = 0; j < board.length; j++) {
-  // 	//j is the curr box
-  // 	if (board[j].count == 1) {
-  // 		console.log("count was 1, making random move in this box")
-  // 		//check for attached boxes, if any of them are 2, continue
-  // 		was_two = 000
-  // 		attached = getAttached(j)
-  // 		for (i in attached) {
-  // 			if (board[i].count == 2) {
-  // 				was_two = 111
-  // 				break
-  // 			}
-  // 		}
+  for (let j = 0; j < board.length; j++) {
+  	//j is the curr box
+  	if (board[j].count == 1) {
+  		console.log("count was 1, making random move in this box")
+  		//check for attached boxes, if any of them are 2, continue
+  		was_two = 000; //flag to keep track of if neighbours have 2
+  		attached = getAttached(j)
+  		for (i in attached) {
+  			if (i != -1) {
+          if(board[i].count == 2)
+  				  was_two = 111;
+  				  break;
+  			} else{
+          continue;
+        }
+  		}
 
-  // 		if (was_two = 111) {
-  // 			continue
-  // 		} else {
-  // 			randomMove(j)
-  // 			return;
-  // 		}
+  		if (was_two = 111) {
+  			continue
+  		} else {
+  			randomMove(j)
+  			return;
+  		}
 
-  // 	}
-  // }
-}
-
-function bestMove() {
-  //make winning line
-  //recursiveky make lines
-  for (i in board)
-    for (j in board) {
-      if (j.count == 3) {
-        j.count++;
-        computerScore++;
-        move = possibleMoves(j)[0];
-        j.sides[move] = 1;
-        //TODO - Implement touching side increment
-      }
+  	} else{
+      //********** CALL RANDOM MOVE WITH A MATH.RANDOM AVAILABLE SPOT IN BOX NOT FILLED  **************
+      randomMove()
     }
+  }
 }
 
-// function randomMove(box_num) {
-// 	randMove = Move;
-// 	randMove.boxNum1 = location;
-// 	const randomMove = board[Math.floor(Math.random() * board.length)];
-// 	passed_in.move1 = randomMove;
-// 	passed_in.boxNum2 = neighbour;
-// 	passed_in.move2 = 1;
+function bestMove(pos) {
+	side = possibleMoves(board[pos])[0];
+	winningMove = Move;
+	winningMove.boxNum1 = pos;
+	winningMove.move1 = side;
+	winningMove.player = "computer";
+ //  winningMove.boxNum2 = pos;
+	// winningMove.move2 = side;
+	//TODO Make A function that changes the neightbor side
+	boardUpdate(winningMove);
+  ComputerMove();
+}
 
-// }
+function randomMove(box_num) {
+	randMove = Move;
+	randMove.boxNum1 = box_num;
+	
+  side = possibleMoves(board[box_num])[0];
+	RandMove = Move;
+	RandMove.boxNum1 = box_num;
+	RandMove.move1 = side;
+	RandMove.player = "computer";
+ //  winningMove.boxNum2 = pos;
+	// winningMove.move2 = side;
+	//TODO Make A function that changes the neightbor side
+	boardUpdate(randMove);
+
+}
 
 function reset() {}
 
